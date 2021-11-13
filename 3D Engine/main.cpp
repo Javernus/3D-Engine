c79561512fe6918b1f2ea686abdf1d1ae82742d1
Jake Jongejans <jake.jongejans@voiceworks.com>
2021-11-13 13:34:33 +0100

    Initial Commit

---
+++ 3D Engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  3D Engine
+//
+//  Created by Jake Jongejans on 13/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
