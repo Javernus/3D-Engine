6b18c03467650460744407e8a36148161777d65d
Jake Jongejans <jake.jongejans@voiceworks.com>
2021-11-13 16:31:17 +0100

    chore(vulkan): Vulkan CreateInstance working.

--- 3D Engine/main.cpp
+++ 3D Engine/main.cpp
@@ -8,7 +8,7 @@
 #include "core.h"
 
 class HelloTriangleApplication {
-  
+
 public:
   void run() {
     initWindow();
@@ -16,30 +16,30 @@ public:
     mainLoop();
     cleanup();
   }
-  
+
 private:
   const uint32_t WIDTH = 800;
   const uint32_t HEIGHT = 600;
   const char** glfwExtensions;
-  
+
   uint32_t glfwExtensionCount = 0;
-  
+
   GLFWwindow* window;
   VkInstance instance;
-  
+
   void initWindow() {
     glfwInit();
-    
+
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    
+
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
   }
-  
+
   void initVulkan() {
     createInstance();
   }
-  
+
   void createInstance() {
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -48,30 +48,31 @@ private:
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_0;
-    
+
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
-    
+
     glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
     createInfo.enabledExtensionCount = glfwExtensionCount;
     createInfo.ppEnabledExtensionNames = glfwExtensions;
     createInfo.enabledLayerCount = 0;
-    
-    std::cout << "> " << vkCreateInstance(&createInfo, nullptr, &instance) << "\n";
-    
-//    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-//        throw std::runtime_error("failed to create instance!");
-//    }
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+    if (result != VK_SUCCESS) {
+      printf("The error: %i\n", result);
+      throw std::runtime_error("failed to create instance!");
+    }
   }
-  
+
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
       glfwPollEvents();
     }
   }
-  
+
   void cleanup() {
     glfwDestroyWindow(window);
     glfwTerminate();
@@ -80,13 +81,13 @@ private:
 
 int main() {
   HelloTriangleApplication app;
-  
+
   try {
     app.run();
   } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
   }
-  
+
   return EXIT_SUCCESS;
 }
